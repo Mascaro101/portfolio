491d68f85e4fe55abfa3f7ae0220aefacc87f9f7
unknown <mascaro400@gmail.com>
2024-06-23 19:06:30 +0200

    Update with working SkyPack URL

--- src/components/Starfield.jsx
+++ src/components/Starfield.jsx
@@ -1,6 +1,6 @@
 // src/components/Starfield.jsx
 import React, { useRef, useEffect } from 'react';
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.165.0/build/three.module.js';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import profilePicture from '../assets/profilePicture.jpg'; 
